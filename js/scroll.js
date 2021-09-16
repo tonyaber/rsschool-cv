fc30e166a31b039dc9c0b83ec7050381f754d219
Antonina Berchuk <tonyaberch@gmail.com>
2021-09-16 20:11:07 +0200

    feat: add highlight, scroll.js, burger_menu.js

---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+const animationTime = 4,
+    scrollBy = 5;
+
+document.querySelectorAll('.header_list a[href*="#"').forEach(anchor => {
+    anchor.addEventListener('click', (evt) => {
+        evt.preventDefault();
+
+        const anchorY = document.querySelector(anchor.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+        const scroll = setInterval(() => {
+            if (scrollBy >= window.pageYOffset - anchorY + 10 && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+                window.scrollBy(0, scrollBy);
+            }
+            else if (scrollBy <= window.pageYOffset - anchorY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+                window.scrollBy(0, -scrollBy);
+            }          
+            else {
+                clearInterval(scroll);
+            }
+        }, animationTime);
+    })
+});
